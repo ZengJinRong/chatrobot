d61ab06cc10c2fc18fa11bcfcf89866ba3d84dcd
zengjinrong <jinrong.zeng@outlook.com>
2018-04-13 20:11:04 +0800

    save APIKey by chatrobot.properties;

--- src/pers/zengjinrong/chatrobot/bean/MessageToSend.java
+++ src/pers/zengjinrong/chatrobot/bean/MessageToSend.java
@@ -1,18 +1,34 @@
 package pers.zengjinrong.chatrobot.bean;
 
-import key.Key;
+import pers.zengjinrong.chatrobot.Main;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * Bean 用于发送消息的JSON格式化
  *
  * @author ZengJinRong
- * @version 1.1
+ * @version 2.0
  */
 public class MessageToSend {
-    private String key = Key.API_KEY;   //图灵机器人APIKey（保密）
-    private String info;                //消息内容
+    private String key;         //图灵机器人APIKey
+    private String info;        //消息内容
 
     public MessageToSend(String info) {
+        if (key == null) {
+            //从配置文件读取APIKey
+            try {
+                InputStream inputStream=MessageToSend.class.getResourceAsStream("/chatrobot.properties");
+                Properties properties=new Properties();
+                properties.load(inputStream);
+                key=properties.getProperty("apikey");
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+        }
         this.info = info;
     }
 
